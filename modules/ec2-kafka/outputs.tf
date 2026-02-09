5d553edea731f94ed950ad545c8ea27f93ad8b19
yseoy0311 <yseoy0311@gmail.com>
2026-02-09 16:55:05 +0900

    remove(modules): ec2 kafka 모듈 삭제

--- modules/ec2-kafka/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "broker_private_ips" {
-  description = "Kafka 브로커들의 내부 IP 목록"
-  value = concat(
-    aws_instance.broker_zone_a[*].private_ip,
-    aws_instance.broker_zone_c[*].private_ip
-  )
-}
